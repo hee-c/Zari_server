4b46ceb980dc9b25632b2a7340c906b80c826e16
hee-c <ways2kim@gmail.com>
2021-05-06 16:31:31 +0900

    Add /auth/login endpoint

---
+++ controllers/auth.controller.js
@@ -0,0 +1,46 @@
+const admin = require('firebase-admin');
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const User = require('../models/User');
+const { accessTokenExpired } = require('../constants');
+
+exports.login = async function (req, res, next) {
+  try {
+    const { email, name, picture } = await admin.auth().verifyIdToken(req.body.id_token);
+    const currentUser = await User.findOne({ email });
+
+    if (currentUser) {
+      res.json({
+        code: 200,
+        message: 'login success',
+        data: {
+          accessToken: jwt.sign(
+            { id: currentUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: accessTokenExpired }
+          ),
+        },
+      });
+    } else {
+      const newUser = await User.create({
+        email,
+        name,
+        picture
+      });
+
+      res.json({
+        code: 200,
+        message: 'login success',
+        data: {
+          accessToken: jwt.sign(
+            { id: newUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: accessTokenExpired }
+          ),
+        },
+      });
+    }
+  } catch (err) {
+    next(createError(500, err));
+  }
+};

bb5d27e3ebb0f321a615880a3cc72f0d9394c8f6
hee-c <ways2kim@gmail.com>
2021-05-06 16:30:17 +0900

    Add app.js logic

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const http = require('http');
+const statuses = require('statuses');
+
+const { authHeader } = require('./constants');
+require('./loader/database');
+require('./loader/firebaseAdmin');
+
+const port = process.env.PORT || '5000';
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors({ exposedHeaders: authHeader }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('./routes'));
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    code: res.status,
+    message: statuses[res.status],
+  });
+});
+
+server.listen(port);

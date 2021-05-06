9575c96dfa2379f0002e31f52a6d2fe566a289a8
hee-c <ways2kim@gmail.com>
2021-05-06 16:38:51 +0900

    Add verifyToken middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+
+exports.verifyUser = async function (req, res, next) {
+  try {
+    await jwt.verify(req.headers.authorization, process.env.JWT_SECRET, async (err, decoded) => {
+      if (err) {
+        return next(createError(401, err));
+      }
+
+      req.userId = decoded.id;
+
+      next();
+    });
+  } catch (err) {
+    next(createError(500, err));
+  }
+};

647f1832aaba2f38dd7ca0f6be91fa8464ec22a6
hee-c <ways2kim@gmail.com>
2021-05-19 15:51:15 +0900

    Fix parse pem key error in aws

--- constants/serviceAccountKey.js
+++ constants/serviceAccountKey.js
@@ -1,12 +1,12 @@
 module.exports = {
-  firebaseType: process.env.TYPE,
-  fireebaseProjectId: process.env.PROJECT_ID,
-  firebasePrivateKeyId: process.env.PRIVATE_KEY_ID,
-  firebasePrivateKey: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
-  firebaseClientEmail: process.env.CLIENT_EMAIL,
-  firebaseClientId: process.env.CLIENT_ID,
-  firebaseAuthUrl: process.env.AUTH_URI,
-  firebaseTokenUrl: process.env.TOKEN_URI,
-  firebaseAuthProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  firebaseClientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 }
--- loader/firebaseAdmin.js
+++ loader/firebaseAdmin.js
@@ -1,46 +1,6 @@
 const admin = require('firebase-admin');
-
-const {
-  firebaseType,
-  fireebaseProjectId,
-  firebasePrivateKeyId,
-  firebasePrivateKey,
-  firebaseClientEmail,
-  firebaseClientId,
-  firebaseAuthUrl,
-  firebaseTokenUrl,
-  firebaseAuthProviderX509CertUrl,
-  firebaseClientX509CertUrl,
-} = require('../constants/serviceAccountKey');
-
-const serviceAccount = {
-  type: firebaseType,
-  project_id: fireebaseProjectId,
-  private_key_id: firebasePrivateKeyId,
-  private_key: firebasePrivateKey,
-  client_email: firebaseClientEmail,
-  client_id: firebaseClientId,
-  auth_uri: firebaseAuthUrl,
-  token_uri: firebaseTokenUrl,
-  auth_provider_x509_cert_url: firebaseAuthProviderX509CertUrl,
-  client_x509_cert_url: firebaseClientX509CertUrl,
-};
+const serviceAccountKey = require('../constants/serviceAccountKey');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccountKey)
 });
-
-// admin.initializeApp({
-//   credential: admin.credential.cert({
-//     type: process.env.TYPE,
-//     project_id: process.env.PROJECT_ID,
-//     private_key_id: process.env.PRIVATE_KEY_ID,
-//     private_key: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
-//     client_email: process.env.CLIENT_EMAIL,
-//     client_id: process.env.CLIENT_ID,
-//     auth_uri: process.env.AUTH_URI,
-//     token_uri: process.env.TOKEN_URI,
-//     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-//     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
-//   })
-// });
